f37cacede5496a7d71bd21ca522ad5e7da8d4e27
Aryan karma <aryankarma29@gmail.com>
2024-05-15 21:59:50 +0530

    python new

---
+++ 1.py
@@ -0,0 +1,37 @@
+#PRACTICE
+
+#1st QUESTION - WRITE A PROGRAM TO INPUT 2 NUMBERS AND PRINT THEIR SUM.
+
+#SOLUTION
+
+# first = int(input("enter first :"))
+# second = int(input("enter second :"))
+# print("sum", first + second)
+
+#2nd QUESTION - WRITE A PROGRAM TO INPUT SIDE OF SQUARE AND PRINT ITS AREA.
+
+#ANSWERE
+
+# side = float(input("enter side : "))
+# print("area of square is : ",side*side)
+
+#3 QUESTION - WRITE A PROGRAM TO INPUT 2 FLOATING POINT NUMBERS & PRINT THEIR AVG.
+
+#ANSWERE
+
+# first = float(input("enter first :"))
+# second = float(input("enter second :"))
+# print("avg is",(first+second)/2)
+
+#4th QUESTION - WRITE A PROGRAM TO INPUT 2 INT NUMBERS , A AND B. PRINT TRUE IF A IS GREATER THEN OR EQUAL TO B . IF NOT PRINT FALSE
+
+#ANSWERE
+
+# first = int(input("enter first :"))
+# second = int(input("enter second :"))
+# print("answere is",first>=second)
+
+#STRINGS
+
+#STRING IS A DATA TYPE THAT STORES A SEQUENCE OF CHARACTERS.
+
---
+++ 3.py
@@ -0,0 +1,6 @@
+#CHAPTER THREE BIGENS NOW
+
+#TOPICS = TUPLES AND LISTS
+
+#LISTS IN PYTHON
+#lists is a built in data type that stores set of values
